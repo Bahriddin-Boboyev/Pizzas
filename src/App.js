8a990b7522294417bf54c253641b30e13d46e6e2
scoderr <BahriddinBoboyev997@gmail.com>
2023-08-03 18:52:45 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/header/Navbar";
+import { Home } from "./pages";
 
 const App = () => {
-  return <div>Пицца</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
