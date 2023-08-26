a655dc0bf36ce4003a0e4be1a0614f73f7f26a9c
scoderr <BahriddinBoboyev997@gmail.com>
2023-08-26 16:52:42 +0500

    basket componet ga o'zgartirishlar kiritildi.

--- src/components/main/basket-right.jsx
+++ src/components/main/basket-right.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./main.scss";
 import { Link } from "react-router-dom";
-
 import {
   cartSlice,
   storeTotalCost,
--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import img from "../img/up-cart-1.svg";
 import "./index.scss";
+import { cartSlice, clickStoreProduct, storeItemsCount } from "../helpers";
 
 const Basket = ({ context, getStoreItems }) => {
   let prods = JSON.parse(localStorage.getItem("cart"));
@@ -19,47 +19,61 @@ const Basket = ({ context, getStoreItems }) => {
     <div className="container basket">
       <div className="basket__box">
         <h1 className="basket__title">Ваш заказ</h1>
-
-        <ul className="basket__list">
-          <li className="basket__item">
-            <div className="basket__img-desc--box">
-              <div className="basket__img--block">
-                <img src={img} alt="img" />
-              </div>
-              <div className="basket__description--block">
-                <h3>Пепперони по-деревенски</h3>
-                <p>Традиционное тесто, 23 см</p>
-              </div>
+        {!prods?.length ? (
+          <div className="basket__not-fount--box">
+            <div>
+              <h1>Товар не найден!</h1>
             </div>
-            <div className="basket__button-price--box">
-              <div>
-                <button>-</button>
-                <span>1</span>
-                <button>+</button>
-              </div>
-              <h4>399 ₽</h4>
-            </div>
-          </li>
-          <li className="basket__item">
-            <div className="basket__img-desc--box">
-              <div className="basket__img--block">
-                <img src={img} alt="img" />
-              </div>
-              <div className="basket__description--block">
-                <h3>Пепперони по-деревенски</h3>
-                <p>Традиционное тесто, 23 см</p>
-              </div>
-            </div>
-            <div className="basket__button-price--box">
-              <div>
-                <button>-</button>
-                <span>1</span>
-                <button>+</button>
-              </div>
-              <h4>399 ₽</h4>
-            </div>
-          </li>
-        </ul>
+          </div>
+        ) : (
+          <ul className="basket__list">
+            {products?.map((prod) =>
+              storeItemsCount(prod._id, prods) > 0 ? (
+                <li className="basket__item" key={prod._id}>
+                  <div className="basket__img-desc--box">
+                    <div className="basket__img--block">
+                      <img src={prod.image} alt={prod.name} />
+                    </div>
+                    <div className="basket__description--block">
+                      <h3>{cartSlice(prod.description, 40, 0, 40)}</h3>
+                      <p>{cartSlice(prod.description, 30, 0, 30)}</p>
+                    </div>
+                  </div>
+                  <div className="basket__button-price--box">
+                    <div>
+                      <button
+                        onClick={() =>
+                          clickStoreProduct(
+                            { item: prod, type: "decrement" },
+                            prods,
+                            getStoreItems,
+                          )
+                        }
+                      >
+                        -
+                      </button>
+                      <span>{storeItemsCount(prod._id, prods)}</span>
+                      <button
+                        onClick={() =>
+                          clickStoreProduct(
+                            { item: prod, type: "increment" },
+                            prods,
+                            getStoreItems,
+                          )
+                        }
+                      >
+                        +
+                      </button>
+                    </div>
+                    <h4>{prod.price} ₽</h4>
+                  </div>
+                </li>
+              ) : (
+                ""
+              ),
+            )}
+          </ul>
+        )}
       </div>
     </div>
   );
