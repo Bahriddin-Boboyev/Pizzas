a079f3c85d3767d93832ae61aa2055313313983e
scoderr <BahriddinBoboyev997@gmail.com>
2023-09-28 02:12:39 +0500

    projectga lazy load images qo'shildi

--- src/components/header/cart-down.jsx
+++ src/components/header/cart-down.jsx
@@ -1,25 +1,25 @@
-import React from "react";
 import cart1 from "../../img/cart-down-1.svg";
 import cart2 from "../../img/cart-down-2.svg";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const CartDown = () => {
   return (
     <div className="cartDown">
       <ul className="cartDown__list" id="style-2">
         <li className="cartDown__item">
-          <img src={cart1} alt="img" />
+          <LazyLoadImage src={cart1} alt="img-1" effect="blur" />
           <h3>3 средние пиццы от 999 рублей</h3>
         </li>
         <li className="cartDown__item cartDown__item-2">
-          <img src={cart2} alt="img" />
+          <LazyLoadImage src={cart2} alt="img-2" effect="blur" />
           <h3>Кэшбек 10% на самовывоз (доставка)</h3>
         </li>
         <li className="cartDown__item">
-          <img src={cart1} alt="img" />
+          <LazyLoadImage src={cart1} alt="img-3" effect="blur" />
           <h3>3 средние пиццы от 999 рублей</h3>
         </li>
         <li className="cartDown__item cartDown__item-2">
-          <img src={cart2} alt="img" />
+          <LazyLoadImage src={cart2} alt="img-1" effect="blur" />
           <h3>Кэшбек 10% на самовывоз (доставка)</h3>
         </li>
       </ul>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import "react-toastify/dist/ReactToastify.css";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import "react-loading-skeleton/dist/skeleton.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { DataProvider } from "./context";
