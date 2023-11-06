d0abffe6434fa119d1bec9f32111457141805bd5
scoderr <BahriddinBoboyev997@gmail.com>
2023-11-06 11:07:39 +0500

    update request

--- src/helpers/server-request/getProducts.js
+++ src/helpers/server-request/getProducts.js
@@ -8,11 +8,5 @@ export const getProducts = (axiosFetch, category) => {
     }),
     method: "GET",
     url: "/product",
-    requestConfig: {
-      headers: {
-        Authorization:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjRjZmQxNTcyMWZiNmFmZDk0OWY2NzI3IiwiYWRtaW4iOnRydWV9LCJpYXQiOjE2OTE1MDE2MTIsImV4cCI6MTY5MTU4ODAxMn0.X2RsUeAkbIc1ug0N3ZXMU8eDC9DAxt6HMtkc_fBHtc4",
-      },
-    },
   });
 };
