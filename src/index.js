b0a370e049b2dba37d1f4810f718c2154c02887c
scoderr <BahriddinBoboyev997@gmail.com>
2024-02-19 09:56:15 +0500

    site add Speed Insights

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.scss";
-import "react-toastify/dist/ReactToastify.css";
-import "react-lazy-load-image-component/src/effects/blur.css";
-import "react-loading-skeleton/dist/skeleton.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { DataProvider } from "./context";
+import './index.scss';
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import 'react-loading-skeleton/dist/skeleton.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { BrowserRouter } from 'react-router-dom';
+import { DataProvider } from './context';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <DataProvider>
       <App />
+      <SpeedInsights />
     </DataProvider>
   </BrowserRouter>,
 );
